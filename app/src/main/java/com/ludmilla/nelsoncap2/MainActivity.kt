2fb08d40ed52f3532cf247c3d2a57898a8116b28
ludmillasilva <ludmilla.silva44@gmail.com>
2022-01-21 16:56:53 -0300

    Capitulo 2 - Layouts - livro do Nelson Glauber

---
+++ app/src/main/java/com/ludmilla/nelsoncap2/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ludmilla.nelsoncap2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
